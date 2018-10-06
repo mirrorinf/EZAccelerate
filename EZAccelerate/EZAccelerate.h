946474a0b0631d721ba3c39e2c2004ce277c2434
mirrorinf <mirrorinf@icloud.com>
2018-10-06 10:08:42 +0800

    Initial Commit

---
+++ EZAccelerate/EZAccelerate.h
@@ -0,0 +1,19 @@
+//
+//  EZAccelerate.h
+//  EZAccelerate
+//
+//  Created by ChuXiaoMin on 2018/10/6.
+//  Copyright © 2018 ChuXiaoMin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EZAccelerate.
+FOUNDATION_EXPORT double EZAccelerateVersionNumber;
+
+//! Project version string for EZAccelerate.
+FOUNDATION_EXPORT const unsigned char EZAccelerateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZAccelerate/PublicHeader.h>
+
+
